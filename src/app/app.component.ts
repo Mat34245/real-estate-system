61fa021e4aeade510e31583556fcd91abb6fd26b
Zosia Samosia <Zosia@Samosia.pl>
2026-01-08 16:33:11 +0100

    implement form for user input and display character count

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  
+export class AppComponent { 
+
+  protected firstname: string = '';
+  protected liczba_liter: number = 0;
+ 
+  protected onSubmit(data:any): void {
+    console.log(data.value.firstname);
+    this.firstname = data.value.firstname;
+    ;
+    
+    
+    
+  }
 }
+
+
+
+
